a8d5b28d31e109382a9e27ae89b97e34af550ebc
EgemenCiftci <egemenciftci@gmail.com>
2021-04-05 09:33:37 +0300

    2 files modified

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
+import { DragAndDropService } from "../drag-and-drop.service";
 import { DragAndDropComponent } from "../drag-and-drop/drag-and-drop.component";
 
 @Component({
@@ -7,14 +8,36 @@ import { DragAndDropComponent } from "../drag-and-drop/drag-and-drop.component";
   styleUrls: ["./card.component.css"]
 })
 export class CardComponent implements OnInit {
-  @ViewChild(DragAndDropComponent) dd: DragAndDropComponent;
+  @ViewChild(DragAndDropComponent) dragAndDropComponent: DragAndDropComponent;
   @Input() name: string;
   @Input() showInfo = true;
   @Input() showCrosshair = true;
   @Input() allowDrag = true;
   @Input() allowDrop = true;
 
-  constructor() {}
+  constructor(public dragAndDropService: DragAndDropService) {}
 
   ngOnInit() {}
+
+  onpointerdown(e: any) {
+    if (this.allowDrag && e.button === 0 && e.buttons === 1) {
+      this.dragAndDropService.reset();
+      this.dragAndDropService.isMouseDown = true;
+      this.dragAndDropService.fromCard = this.name;
+      this.dragAndDropService.fromX = e.offsetX;
+      this.dragAndDropService.fromY = e.offsetY;
+      this.dragAndDropComponent.canvas.nativeElement.setPointerCapture(e.pointerId);
+    }
+  }
+
+  onpointermove(e: any) {
+    if (this.dragAndDropService.isMouseDown) {
+      this.dragAndDropService.isDragging = true;
+      this.dragAndDropService.isInDragDropMode = true;
+      this.dragAndDropComponent.clearCanvas();
+      if (this.showCrosshair) {
+        this.dragAndDropComponent.drawCrosshair(e.offsetX, e.offsetY);
+      }
+    }
+  }
 }
